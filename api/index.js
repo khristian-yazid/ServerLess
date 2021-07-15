e24411cae2ca69e36fa6e9a47279d762bfe6819a
Khristian Ali <khristian-yazid1@hotmail.com>
2021-07-14 23:17:13 -0400

    Repo init

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const meals = require("./routes/meals");
+const orders = require("./routes/orders");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use("/api/meals", meals);
+app.use("/api/orders", orders);
+module.exports = app;
---
+++ api/models/Orders.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Orders = mongoose.model('Order', new Schema({
+    meal_id:{type: Schema.Types.ObjectId, ref: 'Meals'},
+    user_id: String,
+}));
+
+module.exports = Orders;
